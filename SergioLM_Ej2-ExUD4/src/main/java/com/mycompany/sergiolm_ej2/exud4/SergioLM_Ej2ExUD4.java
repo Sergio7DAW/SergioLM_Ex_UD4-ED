5280dd5da5d164914a68a363cb4246275c63a87d
Sergio7DAW <sergio.daw9@gmail.com>
2025-03-25 14:09:25 +0100

    second commit

---
+++ SergioLM_Ej2-ExUD4/src/main/java/com/mycompany/sergiolm_ej2/exud4/SergioLM_Ej2ExUD4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sergiolm_ej2.exud4;
+
+/**
+ *
+ * @author 1DAW2425-09
+ */
+public class SergioLM_Ej2ExUD4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
